6f75fba2532000838c7fe5031ef59dbfd34cf34e
sharanAmit <amitsharan120977@gmail.com>
2022-04-13 21:03:26 +0530

    youtube_player_iframe removed

--- packages/youtube_player_iframe/lib/src/players/platform_view_stub.dart
+++
@@ -1,6 +0,0 @@
-///
-// ignore: camel_case_types
-class platformViewRegistry {
-  /// This class is just a stub to make the Flutter analyzer happy about platformViewRegistry.
-  static void registerViewFactory(String viewId, dynamic cb) {}
-}
